6ca6236cd0af9947608d3c8111f8958d57ae73f3
Aakuz <lastkingkhan786@email.com>
2026-06-29 14:03:50 +0545

    Fix GitHub Pages deployment config

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-// src/main.jsx
+# Update main.jsx to use HashRouter properly
+@"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { CartProvider } from './context/CartContext'
 import { DataProvider } from './context/DataContext'
@@ -9,12 +10,13 @@ import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <DataProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </DataProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
+"@ | Out-File -FilePath src/main.jsx -Encoding utf8
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-// vite.config.js
+# Update vite.config.js
+@"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/FUTURE_FS_03/',
   server: {
     port: 3000,
     open: true
   }
 })
+"@ | Out-File -FilePath vite.config.js -Encoding utf8
